137cdb62158d700730812357ae79fa659ef7b091
Dan Staples <dan.staples@riseup.net>
2013-07-25 22:03:14 -0400

    progress towards replacing commotion-service-parser

--- core-browse-services.c
+++ core-browse-services.c
@@ -9,6 +9,8 @@
 #include <net/if.h>
 #include <string.h>
 #include <signal.h>
+#include <regex.h>
+#include <ctype.h>
 
 #include <avahi-core/core.h>
 #include <avahi-core/lookup.h>
@@ -16,6 +18,8 @@
 #include <avahi-common/malloc.h>
 #include <avahi-common/error.h>
 #include <avahi-common/llist.h>
+#include <avahi-common/timeval.h>
+#include <avahi-common/watch.h>
 
 #include "escape.h"
 #include "concat.h"
@@ -59,7 +63,16 @@ static void resolve_callback(
     AvahiLookupResultFlags flags,
     void* userdata);
 
-static ServiceInfo *find_service(AvahiIfIndex interface, AvahiProtocol protocol, const char *name, const char *type, const char *domain) {
+static int isNumeric (const char *s)
+{
+  if (s == NULL || *s == '\0' || isspace(*s))
+    return 0;
+  char * p;
+  strtod (s, &p);
+  return *p == '\0';
+}
+
+/*static ServiceInfo *find_service(AvahiIfIndex interface, AvahiProtocol protocol, const char *name, const char *type, const char *domain) {
     ServiceInfo *i;
 
     for (i = services; i; i = i->info_next)
@@ -71,6 +84,16 @@ static ServiceInfo *find_service(AvahiIfIndex interface, AvahiProtocol protocol,
 
             return i;
 
+    return NULL;
+}*/
+
+static ServiceInfo *find_service(const char *name) {
+  ServiceInfo *i;
+  
+  for (i = services; i; i = i->info_next)
+    if (strcasecmp(i->name, name) == 0)
+      return i;
+    
     return NULL;
 }
 
@@ -112,6 +135,13 @@ static void remove_service(ServiceInfo *i) {
     avahi_free(i);
 }
 
+static void expire_service(AvahiTimeout *t, void *userdata) {
+  struct timeval tv;
+  ServiceInfo *i = (ServiceInfo*)userdata;
+  
+  fprintf(stdout, "(Expiration) Expiring service announcement: %s\n",i->name);
+  remove_service(i);
+}
 
 static void print_service(FILE *f, ServiceInfo *service) {
     char a[AVAHI_ADDRESS_STR_MAX];
@@ -143,12 +173,16 @@ void sig_handler(int signal) {
         fprintf(stderr, "Could not open %s. Using stdout instead.\n", g_filename);
         f = stdout;
     }
+    
+    /* TODO: write to UCI depending on cmdline flag */
 
     for (i = services; i; i = i->info_next) {
         if (i->resolved)
             print_service(f, i);
     }
 
+    // TODO: check known_applications list, approved or blacklisted
+    
     if (f != stdout) {
         fclose(f);
     }
@@ -167,9 +201,21 @@ static void resolve_callback(
     AvahiStringList *txt,
     AvahiLookupResultFlags flags,
     void* userdata) {
-
+    
     ServiceInfo *i = (ServiceInfo*)userdata;
+    AvahiStringList *txt_entry;
+    char *txt_str, *expiration_str;
+    regex_t fingerprint_re, signature_re;
+    const char fingerprint_pattern[] = "^fingerprint=[[:xdigit:]]{64}$";
+    const char signature_pattern[] = "^signature=[[:xdigit:]]{128}$";
+    struct timeval tv;
+    
     assert(r);
+    if (regcomp(&fingerprint_re, fingerprint_pattern, REG_NEWLINE | REG_EXTENDED | REG_NOSUB) ||
+      regcomp(&signature_re, signature_pattern, REG_NEWLINE | REG_EXTENDED | REG_NOSUB)) {
+      fprintf(stderr,"(Resolver) Failed to compile regexes\n");
+      return;
+    }
 
     switch (event) {
         case AVAHI_RESOLVER_FAILURE:
@@ -177,20 +223,98 @@ static void resolve_callback(
             break;
 
         case AVAHI_RESOLVER_FOUND: {
-            if (!(i->txt = txt_list_to_string(txt))) {
-                fprintf(stderr, "Could not resolve the text field!\n");
-            }
-                
+	    /*int match = 0;
+	    regex_t application_re, ipaddr_re, icon_re, desc_re, ttl_re, exp_re;
+	    const char application[] = "^application=.+$";
+	    const char ttl[] = "^ttl=\d+$";
+	    const char exp[] = "^expiration=\d+$";
+	    const char ipaddr[] = "^(?:[[:alpha:]]+://)?([^/:]+)";
+	    const char icon[] = "^icon=.+$";
+	    const char desc[] = "^description=.+$";
+	    if (regcomp(&application_re, application, REG_NEWLINE | REG_EXTENDED | REG_NOSUB) || 
+	      regcomp(&ttl_re, ttl, REG_NEWLINE | REG_EXTENDED | REG_NOSUB) || 
+	      regcomp(&exp_re, exp, REG_NEWLINE | REG_EXTENDED | REG_NOSUB) || 
+	      regcomp(&ipaddr_re, ipaddr, REG_NEWLINE | REG_EXTENDED) || 
+	      regcomp(&icon_re, icon, REG_NEWLINE | REG_EXTENDED | REG_NOSUB) || 
+	      regcomp(&desc_re, desc, REG_NEWLINE | REG_EXTENDED | REG_NOSUB)) {
+	      fprintf(stderr,"(Resolver) Failed to compile regexes\n");
+	    }*/
+	  
             avahi_address_snprint(i->address, 
                 sizeof(i->address),
                 address);
-            i->port = port;
-            i->host_name = strdup(host_name);
+	    i->host_name = strdup(host_name);
+	    if (port < 0 || port > 65535) {
+	      fprintf(stderr,"(Resolver) Invalid port: %s\n",name);
+	      break;
+	    }
+	    i->port = port;
+	    
+	    /*char *test = avahi_string_list_to_string(txt);
+	    fprintf(stdout,"%s\n",test);
+	    avahi_free(test);
+	    */
+	    
+	    if (!avahi_string_list_find(txt,"application") ||
+	      !avahi_string_list_find(txt,"icon") ||
+	      !avahi_string_list_find(txt,"description") ||
+	      !avahi_string_list_find(txt,"ttl") ||
+	      !avahi_string_list_find(txt,"expiration") ||
+	      !avahi_string_list_find(txt,"signature") ||
+	      !avahi_string_list_find(txt,"fingerprint")) { // TODO: This might not include local-only apps (w/ TTL == 0)
+	      fprintf(stderr,"(Resolver) Missing TXT field(s): %s\n", name);
+	      break;
+	    }
+	    
+	    txt_entry = avahi_string_list_find(txt,"ttl");
+	    txt_str = avahi_string_list_get_text(txt_entry) + 4*sizeof(char);
+	    if (!isNumeric(txt_str) || atoi(txt_str) < 0) {
+	      fprintf(stderr,"(Resolver) Invalid TTL value: %s -> %s\n",name,txt_str);
+	      break;
+	    }
+	    
+	    txt_entry = avahi_string_list_find(txt,"expiration");
+	    expiration_str = avahi_string_list_get_text(txt_entry) + 11*sizeof(char);
+	    if (!isNumeric(expiration_str) || atoi(expiration_str) < 0) {
+	      fprintf(stderr,"(Resolver) Invalid expiration value: %s -> %s\n",name,expiration_str);
+	      break;
+	    }
+	    
+	    txt_entry = avahi_string_list_find(txt,"fingerprint");
+	    txt_str = avahi_string_list_get_text(txt_entry);
+	    if (regexec(&fingerprint_re, txt_str, 0, NULL, 0)) {
+	      fprintf(stderr,"(Resolver) Invalid fingerprint: %s -> %s\n",name,txt_str);
+	      break;
+	    }
+	    
+	    txt_entry = avahi_string_list_find(txt,"signature");
+	    txt_str = avahi_string_list_get_text(txt_entry);
+	    if (regexec(&signature_re, txt_str, 0, NULL, 0)) {
+	      fprintf(stderr,"(Resolver) Invalid signature: %s -> %s\n",name,txt_str);
+	      break;
+	    }
+
+	    // TODO: check connectivity, using commotiond socket API
+	    
+	    // TODO: verify signature, using commotiond serval key mgmt API
+	    
+	    // TODO: if signature verifies:
+	    avahi_elapse_time(&tv, 1000*atoi(expiration_str), 0);
+	    avahi_simple_poll_get(simple_poll)->timeout_new(avahi_simple_poll_get(simple_poll), &tv, expire_service, i); // create expiration event for service
+	    if (!(i->txt = txt_list_to_string(txt))) {
+	      fprintf(stderr, "Could not resolve the text field!\n");
+	      break;
+	    }
             i->resolved = 1;
         }
     }
     avahi_s_service_resolver_free(i->resolver);
     i->resolver = NULL;
+    if (event == AVAHI_RESOLVER_FOUND && !i->resolved) {
+      remove_service(i);
+    }
+    regfree(&fingerprint_re);
+    regfree(&signature_re);
 }
 static void browse_service_callback(
     AvahiSServiceBrowser *b,
@@ -221,7 +345,8 @@ static void browse_service_callback(
             ServiceInfo *found_service = NULL;
             fprintf(stderr, "(Browser) %s: service '%s' of type '%s' in domain '%s'\n",event == AVAHI_BROWSER_NEW ? "NEW" : "REMOVE", name, type, domain);
 
-            found_service=find_service(interface, protocol, name, type, domain);
+            //found_service=find_service(interface, protocol, name, type, domain);
+	    found_service=find_service(name); // name is fingerprint, so should be unique
             if (event == AVAHI_BROWSER_NEW && !found_service) {
                 /* add the service.
                  */
@@ -286,9 +411,13 @@ static void browse_type_callback(
 int main(int argc, char*argv[]) {
     AvahiServerConfig config;
     AvahiSServiceTypeBrowser *stb = NULL;
+    struct timeval tv;
     int error;
     int ret = 1;
 
+    /* TODO: Parse command line parameters using argp 
+     -u/--uci write out to /etc/config/applications */
+    
     if (argc == 2) {
         /* 
          * we are going to use this for our filename.
@@ -340,7 +469,7 @@ int main(int argc, char*argv[]) {
         fprintf(stderr, "Failed to create service browser: %s\n", avahi_strerror(avahi_server_errno(server)));
         goto fail;
     }
-
+    
     /* Run the main loop */
     avahi_simple_poll_loop(simple_poll);
 
